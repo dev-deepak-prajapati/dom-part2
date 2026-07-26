590b521b18f1dc6af4d20d3012161448c6bea316
dev-deepak-prajapati <dev.deepak.prajapati@gmail.com>
2026-07-26 14:44:03 +0530

    add change Tag name method

--- d6/script.js
+++ d6/script.js
@@ -38,3 +38,42 @@ const divEle = document.createElement("div");
 
 divEle.setAttribute("class", "rotate");
 body.append(divEle);
+
+/**
+ * Question 4: Change an HTML Element's Tag Name
+ * 
+ * Description: Create a function that takes an existing DOM element and changes 
+ * its tag (e.g., from a <div> to a <p>), while perfectly preserving all of its 
+ * child elements, text, event listeners, and attributes (id, class, etc.).
+ */
+
+
+function changeTagName(oldNode, newTagName) {
+  // check node is valid or not
+  if (!(oldNode instanceof Element) || typeof newTagName !== "string") {
+    console.error("Invalid node or tag name provided.");
+    return;
+  }
+
+  // create new tag element
+  const newNode = document.createElement(newTagName);
+
+  ///** moving all childs oldNode to newNode */
+  ///** 1st method: old way*/
+
+  //   while (oldNode.firstChild) {
+  //     newNode.appendChild(oldNode.firstChild);
+  //   }
+
+  ////**2nd method: modern way*/
+  ///** No need for a loop! beacuase it use sprade operator */
+
+  newNode.append(...oldNode.childNodes);
+
+  for (const val of oldNode.attributes) {
+    newNode.setAttribute(val.name, val.value);
+  }
+
+  oldNode.replaceWith(newNode);
+}
+changeTagName(div, "p");
